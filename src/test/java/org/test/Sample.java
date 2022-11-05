c3ecdfcaa8b9e7f12a5435d2b9be95297cf54f5c
Gomathi1303 <ngomathi.13@gmail.com>
2022-11-05 06:56:57 +0530

    project strucure

---
+++ src/test/java/org/test/Sample.java
@@ -0,0 +1,23 @@
+package org.test;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	private void test1() {
+		System.out.println("test1");
+	}
+	
+	@Test
+	private void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	private void test3() {
+		System.out.println("test3");
+	}
+	
+	
+
+}
